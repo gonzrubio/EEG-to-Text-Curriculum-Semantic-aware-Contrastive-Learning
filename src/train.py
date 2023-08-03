3877d4862b51f1b76625da97ab8d99184a3cd4fe
Gonzalo <gonzrubio95@gmail.com>
2023-08-02 22:31:24 -0700

    fix random seed

--- src/train.py
+++ src/train.py
@@ -1,1 +1,14 @@
 """Train an EEG-to-Text decoding model with Curriculum Semantic-aware Contrastive Learning."""
+
+from utils.set_seed import set_seed
+
+
+def main():
+    cfg = {
+        'seed': 312
+         }
+    set_seed(cfg['seed'])
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/set_seed.py
@@ -0,0 +1,20 @@
+"""Fix random seed.
+
+https://wandb.ai/sauravmaheshkar/RSNA-MICCAI/reports/How-to-Set-Random-Seeds-in-PyTorch-and-Tensorflow--VmlldzoxMDA2MDQy
+"""
+
+import os
+import random
+
+import numpy as np
+import torch
+
+
+def set_seed(seed):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(seed)
