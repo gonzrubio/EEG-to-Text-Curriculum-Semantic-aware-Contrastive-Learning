a3eb54f21ad6fadb23b4cddbf3068f8bf69148b2
Gonzalo <gonzrubio95@gmail.com>
2023-08-28 18:35:53 -0700

    separate BrainTranslator into pre-encoder and seq2seq

--- src/model.py
+++ src/model.py
@@ -12,7 +12,46 @@ class BrainTranslator(nn.Module):
 
     Args:
     ----
-        pretrained_seq2seq (nn.Module): Pretrained sequence-to-sequence model
+        pre_encoder: Pre-encoder trained with CSCL
+        pretrained_seq2seq: Pretrained sequence-to-sequence model
+
+    """
+
+    def __init__(self, pre_encoder, pretrained_seq2seq):
+        super(BrainTranslator, self).__init__()
+        self.pre_encoder = pre_encoder
+        self.seq2seq = pretrained_seq2seq
+
+    def forward(self, src, mask_pre_encoder, mask_seq2seq, labels):
+        """Forward pass of the BrainTranslator model.
+
+        Args:
+        ----
+            src (Tensor): Word-level EEG (batch_size, seq_len, input_dim)
+            mask_pre_encoder (Tensor): Input masks (1 is masked, 0 is not)
+            mask_seq2seq (Tensor): ~ Input masks (0 is masked, 1 is not)
+            labels: (Tensor): Target labels
+
+        Returns
+        -------
+            out (Tensor): The decoded EEG.
+
+        """
+        out = self.pre_encoder(src, mask_pre_encoder)
+        out = self.seq2seq(
+            inputs_embeds=out,
+            attention_mask=mask_seq2seq,
+            return_dict=True,
+            labels=labels
+            )
+        return out
+
+
+class BrainTranslatorPreEncoder(nn.Module):
+    """Pre-encoder module for BrainTranslator.
+
+    Args:
+    ----
         input_dim (int): Dimension of input eeg (105*num_bands) (default: 840)
         num_layers (int): Number of layers in the pre-encoder (default: 6)
         nhead (int): Number of heads in multiheadattention (default: 8)
@@ -22,14 +61,13 @@ class BrainTranslator(nn.Module):
     """
 
     def __init__(self,
-                 pretrained_seq2seq,
                  input_dim=840,
                  num_layers=6,
                  nhead=8,
                  dim_pre_encoder=2048,
                  dim_s2s=1024):
 
-        super(BrainTranslator, self).__init__()
+        super(BrainTranslatorPreEncoder, self).__init__()
 
         self.pre_encoder_layer = nn.TransformerEncoderLayer(
             d_model=input_dim,
@@ -37,44 +75,31 @@ class BrainTranslator(nn.Module):
             dim_feedforward=dim_pre_encoder,
             batch_first=True
             )
-
-        self.pre_encoder = nn.TransformerEncoder(
+        self.pre_encoder_transformer = nn.TransformerEncoder(
             self.pre_encoder_layer,
             num_layers=num_layers
             )
-
         self.fc = nn.Linear(
             in_features=input_dim, out_features=dim_s2s, bias=True
             )
 
-        self.seq2seq = pretrained_seq2seq
-
-    def forward(self, src, input_masks_batch, input_masks_invert, labels):
-        """Forward pass of the BrainTranslator model.
+    def forward(self, src, mask_pre_encoder):
+        """Forward pass of the BrainTranslatorPreEncoder model.
 
         Args:
         ----
             src (Tensor): Word-level EEG (batch_size, seq_len, input_dim)
-            input_masks_batch (Tensor): Input masks (0 is masked, 1 is not)
-            input_masks_invert (Tensor): ~ Input masks (1 is masked, 0 is not)
-            labels: (Tensor): Target labels
+            mask_pre_encoder (Tensor): Input masks (1 is masked, 0 is not)
 
         Returns
         -------
             out (Tensor): The decoded EEG.
 
         """
-        out = self.pre_encoder(src, src_key_padding_mask=input_masks_invert)
-
-        out = F.relu(self.fc(out))
-
-        out = self.seq2seq(
-            inputs_embeds=out,
-            attention_mask=input_masks_batch,
-            return_dict=True,
-            labels=labels
+        out = self.pre_encoder_transformer(
+            src, src_key_padding_mask=mask_pre_encoder
             )
-
+        out = F.relu(self.fc(out))
         return out
 
 
@@ -90,20 +115,24 @@ def main():
     dim_pre_encoder = 2048  # hidden dim for pre encoder
     dim_s2s = 1024          # hidden dim for seq2seq model
 
-    config = BartConfig.from_pretrained('facebook/bart-large')
-    s2s = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
-
-    model = BrainTranslator(
-        s2s,
+    pre_encoder = BrainTranslatorPreEncoder(
         input_dim=input_dim,
         num_layers=num_layers,
         nhead=nhead,
         dim_pre_encoder=dim_pre_encoder,
         dim_s2s=dim_s2s
-    ).to(device)
+        )
+
+    num_params = sum(p.numel() for p in pre_encoder.parameters() if p.requires_grad)
+    print(f"{num_params:,} trainable parameters in the pre-encoder")
+
+    config = BartConfig.from_pretrained('facebook/bart-large')
+    s2s = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
+
+    model = BrainTranslator(pre_encoder, s2s).to(device)
 
     num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    print(f"Number of trainable parameters: {num_params:,}")
+    print(f"{num_params:,}trainable parameters in BrainTranslator")
 
     # dummy sample for testing
     batch_size = 64
