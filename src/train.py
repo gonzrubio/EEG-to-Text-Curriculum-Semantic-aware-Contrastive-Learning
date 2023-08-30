3a76d573b647068e073a13b1f73ee25791ea5d0e
Gonzalo <gonzrubio95@gmail.com>
2023-08-29 19:20:39 -0700

    up to forward pass

--- src/train.py
+++ src/train.py
@@ -1,10 +1,11 @@
 """Train an EEG-to-Text decoding model with Curriculum Semantic-aware Contrastive Learning."""
 
+import copy
 import os
 import pickle
+import time
 
 import torch
-import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from transformers import BartTokenizer, BartForConditionalGeneration
@@ -24,135 +25,65 @@ def train_BrainTranslator(
 def train_CSCL(
         model, dataloaders, cscl, loss_fn, optimizer, epochs, device
         ):
-    pass
-    # for level in range(1):
-    #     for data in train_dataloader:
-    #         EEG = data[0]
-    #         sentence = data[-1]
-    #         subject = data[-2]
+    since = time.time()
+    best_model_wts = copy.deepcopy(model.state_dict())
 
-    #         # online sample of contrastive triplets
-    #         E, E_pos, E_neg = cscl.get_triplet(EEG, subject, sentence, level)
-    #         # TODO move to device here
+    for level in range(1):
+        best_loss = 100000000000
 
-    #         print(f'\ncurriculum level {level}')
-    #         print('-------------------')
-    #         print('positive  negative')
-    #         print('--------  --------')
-    #         for e, e_pos, e_neg in zip(E, E_pos, E_neg):
-    #             sim_pos = F.cosine_similarity(
-    #                 e.sum(dim=0) / E[:, 0].count_nonzero(),
-    #                 e_pos.sum(dim=0) / e_pos[:, 0].count_nonzero(),
-    #                 dim=0
-    #                 )
+        for epoch in range(epochs):
+            print(f'Epoch {epoch}/{epochs - 1}')
+            print('-' * 10)
 
-    #             sim_neg = F.cosine_similarity(
-    #                 e.sum(dim=0) / E[:, 0].count_nonzero(),
-    #                 e_neg.sum(dim=0) / e_neg[:, 0].count_nonzero(),
-    #                 dim=0
-    #                 )
-    #             print(f'{sim_pos.item():.4f}    {sim_neg.item():.4f}')
-    # fwd pass triplet through pre-encoder (flag in fwd for pre-encoder only)
-    # compute averaged vector of the outputs of the pre-encoder
-    # compute loss on batch (equation 2) (start with no temp, see how it varies with temp)
-    # verify loss @ init -log(1/n_classes)
-    # compute input-indepent baseline.
-    # overfit small(est) batch
-    # since = time.time()
+            for phase in ['train', 'dev']:
+                if phase == 'train':
+                    model.train()
+                else:
+                    model.eval()
 
-    # best_model_wts = copy.deepcopy(model.state_dict())
-    # best_loss = 100000000000
+                running_loss = 0.0
+                for EEG, _, _, _, _, _, subject, sentence in dataloaders[phase]:
+                    # online sample of contrastive triplets
+                    E, E_pos, E_neg = cscl.get_triplet(
+                        EEG, subject, sentence, level
+                        )
+                    E = E.to(device)
+                    E_pos = E_pos.to(device)
+                    E_neg = E_neg.to(device)
 
-    # for epoch in range(epochs):
-    #     print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-    #     print('-' * 10)
+                    # forward
+                    # with torch.set_grad_enabled(phase == 'train'):
+                        # seq2seqLMoutput = model(input_embeddings_batch, input_masks_batch, input_mask_invert_batch, target_ids_batch)
+                        # compute averaged vector of the outputs of the pre-encoder
+                        # compute loss (equation 2)
 
-    #     # Each epoch has a training and validation phase
-    #     for phase in ['train', 'dev']:
-    #         if phase == 'train':
-    #             model.train()  # Set model to training mode
-    #         else:
-    #             model.eval()   # Set model to evaluate mode
+                        # backward + optimize only if in training phase
+                        # if phase == 'train':
+                        #     # with torch.autograd.detect_anomaly():
+                        #     optimizer.zero_grad(set to None)
+                        #     loss.backward()
+                        #     optimizer.step()
 
-    #         running_loss = 0.0
+                    # statistics
+                    # running_loss += loss.item() * input_embeddings_batch.size()[0] # batch loss
 
-    #         # Iterate over data.
-    #         for input_embeddings, seq_len, input_masks, input_mask_invert, target_ids, target_mask, sentiment_labels, sent_level_EEG in tqdm(dataloaders[phase]):
+                # epoch_loss = running_loss / dataset_sizes[phase]
+                # print(f'{phase} Loss: {epoch_loss:.4f}')
 
-    #             # load in batch
-    #             input_embeddings_batch = input_embeddings.to(device).float()
-    #             input_masks_batch = input_masks.to(device)
-    #             input_mask_invert_batch = input_mask_invert.to(device)
-    #             target_ids_batch = target_ids.to(device)
-    #             """replace padding ids in target_ids with -100"""
-    #             target_ids_batch[target_ids_batch == tokenizer.pad_token_id] = -100 
+                # deep copy the model
+                # if phase == 'dev' and epoch_loss < best_loss:
+                #     best_loss = epoch_loss
+                #     best_model_wts = copy.deepcopy(model.state_dict())
 
-    #             # zero the parameter gradients
-    #             optimizer.zero_grad()
-
-    #             # forward
-    #             seq2seqLMoutput = model(input_embeddings_batch, input_masks_batch, input_mask_invert_batch, target_ids_batch)
-
-    #             """calculate loss"""
-    #             # logits = seq2seqLMoutput.logits # 8*48*50265
-    #             # logits = logits.permute(0,2,1) # 8*50265*48
-
-    #             # loss = criterion(logits, target_ids_batch_label) # calculate cross entropy loss only on encoded target parts
-    #             # NOTE: my criterion not used
-    #             loss = seq2seqLMoutput.loss # use the BART language modeling loss
-
-    #             # """check prediction, instance 0 of each batch"""
-    #             # print('target size:', target_ids_batch.size(), ',original logits size:', logits.size(), ',target_mask size', target_mask_batch.size())
-    #             # logits = logits.permute(0,2,1)
-    #             # for idx in [0]:
-    #             #     print(f'-- instance {idx} --')
-    #             #     # print('permuted logits size:', logits.size())
-    #             #     probs = logits[idx].softmax(dim = 1)
-    #             #     # print('probs size:', probs.size())
-    #             #     values, predictions = probs.topk(1)
-    #             #     # print('predictions before squeeze:',predictions.size())
-    #             #     predictions = torch.squeeze(predictions)
-    #             #     # print('predictions:',predictions)
-    #             #     # print('target mask:', target_mask_batch[idx])
-    #             #     # print('[DEBUG]target tokens:',tokenizer.decode(target_ids_batch_copy[idx]))
-    #             #     print('[DEBUG]predicted tokens:',tokenizer.decode(predictions))
-
-    #             # backward + optimize only if in training phase
-    #             if phase == 'train':
-    #                 # with torch.autograd.detect_anomaly():
-    #                 loss.backward()
-    #                 optimizer.step()
-
-    #             # statistics
-    #             running_loss += loss.item() * input_embeddings_batch.size()[0] # batch loss
-    #             # print('[DEBUG]loss:',loss.item())
-    #             # print('#################################')
-
-    #         if phase == 'train':
-    #             scheduler.step()
-
-    #         epoch_loss = running_loss / dataset_sizes[phase]
-
-    #         print('{} Loss: {:.4f}'.format(phase, epoch_loss))
-
-    #         # deep copy the model
-    #         if phase == 'dev' and epoch_loss < best_loss:
-    #             best_loss = epoch_loss
-    #             best_model_wts = copy.deepcopy(model.state_dict())
-    #             '''save checkpoint'''
-    #             torch.save(model.state_dict(), checkpoint_path_best)
-    #             print(f'update best on dev checkpoint: {checkpoint_path_best}')
-    #     print()
+            # print()
 
     # time_elapsed = time.time() - since
-    # print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    # print('Best val loss: {:4f}'.format(best_loss))
-    # torch.save(model.state_dict(), checkpoint_path_last)
-    # print(f'update last checkpoint: {checkpoint_path_last}')
+    # print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+    # print(f'Best val loss: {best_loss:4f}')
 
-    # # load best model weights
+    # load best model weights
     # model.load_state_dict(best_model_wts)
-    # return model
+    return model
 
 
 def main():
