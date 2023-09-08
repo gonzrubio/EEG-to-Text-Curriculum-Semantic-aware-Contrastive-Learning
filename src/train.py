c1046f0e0a4d3c46abb9156af30d848fc8a75b0b
Gonzalo <gonzrubio95@gmail.com>
2023-09-07 18:54:10 -0700

    fix bug - loss denominator (naive implementation)

--- src/train.py
+++ src/train.py
@@ -59,20 +59,19 @@ def train_CSCL(
                         h = torch.mean(out, dim=1)
                         h = h.view(-1, 3, h.shape[-1])
 
-                        temp = 1e-1
+                        temp = 1
 
                         num = torch.exp(
                             F.cosine_similarity(h[:, 0, :], h[:, 1, :], dim=1) / temp
                             )
 
-                        denom = 0
+                        denom = torch.empty_like(num, device=num.device)
                         for j in range(E.size(0)):
-                            for kk in range(1, 3):
-                                denom += torch.exp(
-                                    F.cosine_similarity(
-                                        h[:, 0, :], h[:, kk, :], dim=1
-                                        ) / temp
-                                    )
+                            denomjj = 0
+                            for jj in range(E.size(0)):
+                                denomjj += torch.exp(F.cosine_similarity(h[j, 0, :], h[jj, 1, :], dim=0) / temp)
+                                denomjj += torch.exp(F.cosine_similarity(h[j, 0, :], h[jj, 2, :], dim=0) / temp)
+                            denom[j] = denomjj
 
                         loss = -torch.log(num / denom).mean()
                         # print(f'{epoch}.{batch} {phase} Loss: {loss:.4f}')
@@ -110,7 +109,7 @@ def main():
         'eeg_type_choice': 'GD',
         'bands_choice': 'ALL',
         'dataset_setting': 'unique_sent',
-        'batch_size': 32,  # 32
+        'batch_size': 1,
         'shuffle': False,
         'input_dim': 840,
         'num_layers': 6,
@@ -118,7 +117,7 @@ def main():
         'dim_pre_encoder': 2048,
         'dim_s2s': 1024,
         'temp': 1e-5,
-        'lr_pre': 3e-4,
+        'lr_pre': 1e-6,
         'epochs_pre': 1,
         'lr': 2e-5,
         'epochs': 1
